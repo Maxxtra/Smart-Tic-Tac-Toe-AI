ccc9ef70f7e8de5e9a674b92fd11e17e172d27b2
Maxxtra <83414814+Maxxtra@users.noreply.github.com>
2021-10-15 00:11:01 +0300

    Add files via upload

---
+++ tic_tac_toe/test.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include "qmlapplicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QmlApplicationViewer viewer;
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationLockLandscape);
+    viewer.setMainQmlFile(QLatin1String("qml/tic-tac-toe/tic-tac-toe.qml"));
+    viewer.showExpanded();
+
+    return app.exec();
+}
